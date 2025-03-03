062645339baa7bc8c10a8c45453e6f15841d83a8
Miguel Contreras <79333663+Miguel-oc-dev@users.noreply.github.com>
2025-03-02 18:12:28 -0600

    feat: Creación de la API REST para el blog

---
+++ api/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to MongoDB"))  // Función callback
+    .catch((err) => console.error("Error al conectar a MongoDB:", err));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    }, 
+    filename:(req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({ storage:storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("El archivo ha sido actualizado")
+});
+
+    app.use("/api/auth", authRoute);
+    app.use("/api/users", userRoute);
+    app.use("/api/posts", postRoute);
+    app.use("/api/categories", categoryRoute);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`El servidor está corriendo en el puerto ${PORT}...`);
+});
